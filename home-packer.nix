7717446e2cc03e3fd2e29b19574ba045aef4d6f0
whs <hswongac@gmail.com>
2024-05-16 22:16:30 +0800

    Fix

--- home-packer.nix
+++ home-packer.nix
@@ -8,7 +8,7 @@
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "packer";
-  home.homeDirectory = "/home/whs";
+  home.homeDirectory = "/home/packer";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
